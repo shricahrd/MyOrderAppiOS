f0b7467a8f908e8c771d2b43f2004335547c3ccd
rakesh <rahulb2cinfosolutions@gmail.com>
2020-09-30 17:21:38 +0530

    temp

---
+++ MyOrderApp/MyCart/ColorSizeListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ColorSizeListCell.swift
+//  MyOrderApp
+//
+//  Created by Apple on 9/26/20.
+//  Copyright © 2020 rakesh. All rights reserved.
+//
+
+import UIKit
+
+class ColorSizeListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
